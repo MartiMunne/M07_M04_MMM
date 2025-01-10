5a969c8c235daf09d85d401b177ee2af101afecb
MartíMM <marti.munne.7e7@itb.cat>
2025-01-10 12:26:36 +0100

    Unity Project 2

--- Assets/Scripts/ItemScript.cs
+++ Assets/Scripts/ItemScript.cs
@@ -1,15 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Timeline.Actions.MenuPriority;
 
 public class ItemScript : MonoBehaviour, ICollectables
 {
     public int ID;
     public Sprite Sprite;
-
+    private void Awake()
+    {
+        if (PlayerPrefs.HasKey("Item" + ID) && PlayerPrefs.GetInt("Item" + ID) == 1)
+        {
+            LoadItem();
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerPrefs.SetInt("Item" + ID, 1);
+    }
     public void Collect()
     {
         GameManager.gameManager.ItemCollected(Sprite, ID);
         Destroy(gameObject);
+    }
+    public void LoadItem()
+    {
+        GameManager.gameManager.ItemCollected(Sprite, ID);
+        gameObject.SetActive(false);
     }
 }

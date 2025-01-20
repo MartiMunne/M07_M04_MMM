4c2c6d51d0da4908b8fa3b2ddcccd1ead8fbf496
MartíMM <marti.munne.7e7@itb.cat>
2025-01-20 19:59:26 +0100

    Unity Project 4

--- Assets/Scripts/NpcScript.cs
+++ Assets/Scripts/NpcScript.cs
@@ -3,21 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 
-public class SellerScript : MonoBehaviour
+public class NpcScript : MonoBehaviour
 {
    public CinemachineVirtualCamera VCamDisable;
    public CinemachineVirtualCamera VCamEnable;
    public GameObject UI;
+   public GameObject HUD;
    private PlayerMover _playerMover;
-   [SerializeField] Animator _animatorSeller;
-   private bool _canBuy = true;
+   [SerializeField] Animator _animatorSpeaker;
+   private bool _canSpeak = true;
    private float time = 1f;
 
    private  void OnTriggerEnter(Collider other)
    {
-    if(_canBuy)
-        {
-        _animatorSeller.SetBool("isSelling", true);
+        _animatorSpeaker.SetBool("isSpeaking", true);
         VCamDisable.gameObject.SetActive(false);
         VCamEnable.gameObject.SetActive(true);
         Camera.main.GetComponent<CinemachineBrain>().enabled = true;
@@ -25,26 +24,27 @@ public class SellerScript : MonoBehaviour
         _playerMover = other.GetComponent<PlayerMover>();
         _playerMover.canMove = false;
         UI.SetActive(true);
-        _canBuy = false;
-        }
+        HUD.SetActive(false);
+        _canSpeak = false;
    }
    private void OnTriggerExit(Collider other) 
    {
         StartCoroutine(WaitForABit());
    }
-    public void ExitStore()
+    public void ExitDialog()
     {
-        _animatorSeller.SetBool("isSelling", false);
+        _animatorSpeaker.SetBool("isSpeaking", false);
         _playerMover.canMove = true;
         VCamDisable.gameObject.SetActive(true);
         VCamEnable.gameObject.SetActive(false);
         Camera.main.GetComponent<CinemachineBrain>().enabled = false;
         Camera.main.cullingMask |= (1 << 8);
         UI.SetActive(false);
+        HUD.SetActive(true);
     }
     private IEnumerator WaitForABit()
     {
         yield return new WaitForSeconds(time);
-        _canBuy = true;
+        _canSpeak = true;
     }
 }
